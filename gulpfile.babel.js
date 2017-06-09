9200c78912f3efd74fdc3bf5c30a26dbce504c12
Raul Gutierrez <rdgutierrez@gmail.com>
2017-06-09 14:05:09 -0700

    added gulp tasks for tests, not restarting server in dev mode

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,28 @@
 import gulp from 'gulp';
 import path from 'path';
 import rimraf from 'rimraf';
+import child_process from 'child_process';
 
 const $ = require('gulp-load-plugins')();
 
 gulp.task('server:clean', cb => {
   rimraf('./build', () => cb())
-})
+});
 
 gulp.task('server:build',
   gulp.series(
     'server:clean',
     compileServer
-  ));
+  )
+);
 
 gulp.task(
   'server:watch',
   gulp.series(
     'server:build',
     watchServer
-));
+  )
+);
 
 gulp.task(
   'server:dev',
@@ -27,15 +30,29 @@ gulp.task(
     'server:build',
     gulp.parallel(
       watchServer,
-      function nodemon() {
-        return $.nodemon({
-          script: './server.js',
-          watch: 'build'
-        })
-      }
+      runServer
     )
   )
-)
+);
+
+gulp.task(
+  'server:test',
+  gulp.series(
+    'server:build',
+    testServer
+  )
+);
+
+gulp.task(
+  'server:test:dev',
+  gulp.series(
+    'server:build',
+    gulp.parallel(
+      watchServer,
+      runServerTests
+    )
+  )
+);
 
 function compileServer () {
   return gulp.src('./src/server/**/*.js')
@@ -51,4 +68,32 @@ function watchServer () {
   return gulp
     .watch('./src/server/**/*.js', gulp.series(compileServer))
     .on('error', () => {});
+}
+
+function runServer() {
+  return $.nodemon({
+    script: './server.js',
+    watch: 'build',
+    ignore: ['**/__tests']
+  })
+}
+
+function testServer(cb) {
+  child_process.exec('node ./tests.js', (err, stdout, stderr) => {
+    console.log(stdout);
+    console.error(stderr);
+
+    if (err) {
+      cb(new $.util.PluginError('testServer', 'Tests Failed'));
+    } else {
+      cb();
+    }
+  })
+}
+
+function runServerTests() {
+  return $.nodemon({
+    script: './tests.js',
+    watch: 'build'
+  })
 }
